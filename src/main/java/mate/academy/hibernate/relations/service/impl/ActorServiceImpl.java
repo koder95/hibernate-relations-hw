48929e899cc401d4aea07f6dfe8a80189a4b98e4
Kamil Jan Mularski <kammul@outlook.com>
2025-11-10 16:35:43 +0100

    Refactor service implementations to use DAOs
    Updated ActorServiceImpl, CountryServiceImpl, and MovieServiceImpl to use their respective DAO interfaces for data access. Added constructors for dependency injection of DAOs and SessionFactory. Implemented add and get methods in ActorServiceImpl and MovieServiceImpl to delegate to the DAO layer.

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,30 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
+
+    private final ActorDao dao;
+
+    public ActorServiceImpl(ActorDao dao) {
+        this.dao = dao;
+    }
+
+    public ActorServiceImpl(SessionFactory factory) {
+        this(new ActorDaoImpl(factory));
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        return dao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        return dao.get(id).orElseThrow();
     }
 }
